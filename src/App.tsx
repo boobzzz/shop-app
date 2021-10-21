de8b6919b101cc1cf8129b64b630cd18213ac520
Yuriy Borys <iam618384@gmail.com>
2021-10-21 16:56:33 +0300

    Deployment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import { List } from "./screens/List";
 import { Card } from "./screens/Card";
--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,1 +1,2 @@
-export const BASE_EP = "http://localhost:8000/products";
+// export const BASE_EP = "http://localhost:8000/products";
+export const BASE_EP = "https://my-json-server.typicode.com/boobzzz/json-server-db/products";
